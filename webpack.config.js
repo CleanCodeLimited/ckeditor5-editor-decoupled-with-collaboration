dd13e67af612d0203b6d736ae3e390e499e1da3a
Roger Mook <rogermook@gmail.com>
2019-09-27 16:40:22 -0400

    target es5 (needed for angular aot compilation)

--- webpack.config.js
+++ webpack.config.js
@@ -14,80 +14,94 @@ const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' 
 const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
 
 module.exports = {
-        // To enable sourcemap, uncomment this line.
-        // devtool: 'source-map',
-        performance: { hints: false },
+    // To enable sourcemap, uncomment this line.
+    // devtool: 'source-map',
+    performance: { hints: false },
 
-        entry: path.resolve( __dirname, 'src', 'decoupled-editor-with-collaboration.js' ),
+    entry: [
+        require.resolve('regenerator-runtime/runtime.js'),
+        path.resolve( __dirname, 'src', 'decoupled-editor-with-collaboration.js' )
+    ],
 
-        output: {
-                // The name under which the editor will be exported.
-                library: 'DecoupledEditor',
+    output: {
+        // The name under which the editor will be exported.
+        library: 'DecoupledEditor',
 
-                path: path.resolve( __dirname, 'build' ),
-                filename: 'decoupled-editor-with-collaboration.js',
-                libraryTarget: 'umd',
-                libraryExport: 'default'
-        },
+        path: path.resolve( __dirname, 'build' ),
+        filename: 'decoupled-editor-with-collaboration.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
+    },
 
-        optimization: {
-                minimizer: [
-                        new UglifyJsWebpackPlugin( {
-                                // To enable sourcemap, uncomment this line.
-                                // sourceMap: true,
-                                uglifyOptions: {
-                                        output: {
-                                                // Preserve CKEditor 5 license comments.
-                                                comments: /^!/
-                                        }
-                                }
-                        } )
-                ]
-        },
+    optimization: {
+        minimizer: [
+            new UglifyJsWebpackPlugin( {
+                // To enable sourcemap, uncomment this line.
+                // sourceMap: true,
+                uglifyOptions: {
+                    output: {
+                        // Preserve CKEditor 5 license comments.
+                        comments: /^!/
+                    }
+                }
+            } )
+        ]
+    },
 
-        plugins: [
-                new CKEditorWebpackPlugin( {
-                        // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-                        // When changing the built-in language, remember to also change it in the editor configuration (src/ckeditor.js).
-                        language: 'en',
-                        additionalLanguages: 'all'
-                } )
-            // ,
-            //     new webpack.BannerPlugin( {
-            //             banner: [
-            //                     'CKEditor 5 with collaboration features is licensed only under a commercial license and is protected by copyright law.',
-            //                     'For more details about available licensing options please contact us at' +
-            //                     ' https://ckeditor.com/contact/.'
-            //             ].join( '\n' )
-            //     } )
-        ],
+    plugins: [
+        new CKEditorWebpackPlugin( {
+            // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+            // When changing the built-in language, remember to also change it in the editor configuration (src/ckeditor.js).
+            language: 'en',
+            additionalLanguages: 'all'
+        } )
+        // ,
+        //     new webpack.BannerPlugin( {
+        //             banner: [
+        //                     'CKEditor 5 with collaboration features is licensed only under a commercial license and is protected by copyright law.',
+        //                     'For more details about available licensing options please contact us at' +
+        //                     ' https://ckeditor.com/contact/.'
+        //             ].join( '\n' )
+        //     } )
+    ],
 
-        module: {
-                rules: [
-                        {
-                                test: /\.svg$/,
-                                use: [ 'raw-loader' ]
-                        },
-                        {
-                                test: /\.css$/,
-                                use: [
-                                        {
-                                                loader: 'style-loader',
-                                                options: {
-                                                        singleton: true
-                                                }
-                                        },
-                                        {
-                                                loader: 'postcss-loader',
-                                                options: styles.getPostCssConfig( {
-                                                        themeImporter: {
-                                                                themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-                                                        },
-                                                        minify: true
-                                                } )
-                                        },
-                                ]
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: [ 'raw-loader' ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true
                         }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: styles.getPostCssConfig( {
+                            themeImporter: {
+                                themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+                            },
+                            minify: true
+                        } )
+                    },
                 ]
-        }
+            },
+            {
+                test: /ckeditor5-[^\/\\]+[\/\\].*\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [ require( '@babel/preset-env' ) ]
+                        }
+                    }
+                ]
+            },
+        ]
+    }
 };
